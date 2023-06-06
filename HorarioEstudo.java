7a8d3c08a86e480689310b062fa8ef1d894fde79
Lucas <lucastabosa87@outlook.com>
2023-06-05 23:07:52 -0300

    HorarioEstudo

---
+++ HorarioEstudo.java
@@ -0,0 +1,15 @@
+public class HorarioEstudo {
+    private String turno;
+
+    public HorarioEstudo(String turno) {
+        this.turno = turno;
+    }
+
+    public String getTurno() {
+        return turno;
+    }
+
+    public void setTurno(String turno) {
+        this.turno = turno;
+    }
+}
--- UniforOnline.java
+++ UniforOnline.java
@@ -18,7 +18,19 @@ public class UniforOnline {
         aluno aluno2 = new aluno("Feminino");
 
         System.out.println("Sexo do aluno 1: " + aluno1.getSexo());
-        System.out.println("Sexo do aluno 2: " + aluno2.getSexo());  
+        System.out.println("Sexo do aluno 2: " + aluno2.getSexo());
+
+        HorarioEstudo horario1 = new HorarioEstudo("Manhã");
+        HorarioEstudo horario2 = new HorarioEstudo("Tarde");
+        HorarioEstudo horario3 = new HorarioEstudo("Noite");
+
+        System.out.println("Horário de estudo 1: " + horario1.getTurno());
+        System.out.println("Horário de estudo 2: " + horario2.getTurno());
+        System.out.println("Horário de estudo 3: " + horario3.getTurno());
+
+        horario1.setTurno("Tarde");
+        System.out.println("Novo horário de estudo 1: " + horario1.getTurno());
+        System.out.println("Novo horário de estudo 2: " + horario1.getTurno());   
     }
 
 }
