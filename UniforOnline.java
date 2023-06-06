c8639b1ea00df4d64e6ce717570b3ff566ef4874
Lucas <lucastabosa87@outlook.com>
2023-06-05 21:52:38 -0300

    Criação da classe cadastro de aluno

--- UniforOnline.java
+++ UniforOnline.java
@@ -1,5 +1,18 @@
+import java.util.List;
+
 public class UniforOnline {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        CadastroAlunos cadastro = new CadastroAlunos();
+
+        cadastro.adicionarAluno("João");
+        cadastro.adicionarAluno("Maria");
+        cadastro.adicionarAluno("Pedro");
+
+        List<String> alunosCadastrados = cadastro.getAlunos();
+        System.out.println("Alunos cadastrados:");
+
+        for (String aluno : alunosCadastrados) {
+            System.out.println(aluno);
+        }   
     }
 }
